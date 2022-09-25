bd625c2b9c07c55774fc9a09e68cba4af78db735
Charbel Sarkis <charbelsako@gmail.com>
2022-09-25 19:40:58 +0300

    Adds tests for contract methods

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,8 +1,8 @@
 import assert from 'assert';
-import ganache from 'ganache';
 import Web3 from 'web3';
-import { inboxInterface, inboxBytecode } from '../compile';
+import ganache from 'ganache';
 import { describe, it, beforeEach } from 'mocha';
+import { inboxInterface, inboxBytecode } from '../compile';
 
 const web3 = new Web3(ganache.provider());
 
@@ -16,11 +16,20 @@ beforeEach(async () => {
 });
 
 describe('Inbox Contract', () => {
-  it('deploys properly', () => {
+  it('Deploys properly', () => {
     assert.ok(inbox.options.address);
   });
 
-  it('', () => {
+  it('Has default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
 
+  it('Sets message properly', async () => {
+    await inbox.methods
+      .setMessage('Hello World!')
+      .send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hello World!');
   });
 });
